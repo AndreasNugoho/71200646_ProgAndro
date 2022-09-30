e5631dfa0b78df797d5059ed7f74e074d4d971ce
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2022-09-30 13:07:39 +0700

    [Pertemuan 5] <fix orientation, theme, and popup menu>

--- clickRush2/app/src/main/java/com/pemrogandroid/clickrush/MainActivity.kt
+++ clickRush2/app/src/main/java/com/pemrogandroid/clickrush/MainActivity.kt
@@ -5,76 +5,101 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var tombolStart:Button
-    private lateinit var tombolClick:Button
-    private lateinit var score:TextView
-    private lateinit var timeLeft:TextView
-
-    var waktu:Int = 30
-    var click:Int = 0
-
-
-
+    private var LOG_TAG = "UIElement"
+    lateinit var tombolClick: Button
+    lateinit var score:TextView
+    lateinit var timeLeft:TextView
+    var count = 0
+    var countDownTimer: CountDownTimer? = null
+    var remainingTime: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome)
+        val score = findViewById<TextView>(R.id.score)
+        val tombolClick = findViewById<Button>(R.id.tombolClick)
+        val timeLeft = findViewById<TextView>(R.id.timeLeft)
+        tombolClick.setOnClickListener(){
+            val scaleAnim = AnimationUtils.loadAnimation(this,R.anim.scale)
+            tombolClick.startAnimation(scaleAnim)
+            if(count == 0){
+                countDownTimer = object :CountDownTimer(30000,1000){
+                    override fun onTick(mTimeLeftInMillis : Long) {
+                        var second = mTimeLeftInMillis /1000
+                        remainingTime = second
+                        timeLeft.setText("Time left :"+ second)
+                        Log.d(LOG_TAG, "time: $remainingTime")
 
-        tombolStart = findViewById(R.id.tombolStart)
-        tombolClick = findViewById(R.id.tombolClick)
-        score = findViewById(R.id.score)
-        timeLeft = findViewById(R.id.timeLeft)
+                    }
+                    override fun onFinish() {
+                        Toast.makeText(this@MainActivity,"Time Left: 30 Second",Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity,"Time's up! Your score was: $count",Toast.LENGTH_SHORT).show()
+                        count = 0
+                        timeLeft.setText("Waktu Habis")
+                        score.setText("Score: $count")
 
-        tombolStart.isEnabled = true
-        tombolClick.isEnabled = false
+                    }
 
-
-        var timer = object :CountDownTimer(30000,1000){
-            @SuppressLint("SetTextI18n")
-            override fun onTick(millisUntilFinished : Long) {
-                waktu--
-                timeLeft.setText("Waktu: "+waktu)
-
+                }.start()
             }
-
-            override fun onFinish() {
-                tombolClick.isEnabled = false
-                tombolStart.isEnabled = true
-                timeLeft.setText("Waktu: 0")
-                Toast.makeText(this@MainActivity,"waktu habis!",Toast.LENGTH_SHORT).show()
-                Toast.makeText(this@MainActivity,"score anda = $click",Toast.LENGTH_SHORT).show()
+            count++
+            score.setText("Score: $count")
+        }
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("count")
+            remainingTime = savedInstanceState.getLong("remainTime")
+            score.setText("Score: $count")
+            timeLeft.setText("Timeleft: $remainingTime")
+            if (remainingTime > 0L){
+                countDownTimer?.start()
             }
         }
 
-        tombolClick.setOnClickListener(object : View.OnClickListener {
-            @SuppressLint("SetTextI18n")
-            override fun onClick(view: View?) {
-                click++
-                score.setText("Click: "+click)
-            }
+    }
 
-        })
-        tombolStart.setOnClickListener(object : View.OnClickListener {
-            @SuppressLint("SetTextI18n")
-            override fun onClick(view: View?) {
-                timer.start()
-                tombolClick.isEnabled = true
-                tombolStart.isEnabled = false
-                click = 0
-                waktu = 30
-                timeLeft.setText("Waktu: "+waktu)
-                score.setText("Click: "+click)
-            }
-
-        })
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("remainTime",remainingTime)
+        outState.putInt("count",count)
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.about_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val view  = View.inflate(this,R.layout.popup,null)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+        fun dialog(){
+            dialog.show()
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        }
+        when(item.itemId){
+            R.id.about->dialog()
+        }
+        return true
+    }
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        savedInstanceState.getInt("count")
+//        savedInstanceState.getString("time")
+//    }
 }
