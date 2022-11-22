fe83cf1e8ecd4da1e3362ad1112ff4adcd9b1e77
Andreas Nugroho <79098402+AndreasNugoho@users.noreply.github.com>
2022-11-22 14:44:34 +0700

    Update BookmarkDetailsActivity.kt

--- catatanTempat/app/src/main/java/com/pemrogandroid/catatantempat/ui/BookmarkDetailsActivity.kt
+++ catatanTempat/app/src/main/java/com/pemrogandroid/catatantempat/ui/BookmarkDetailsActivity.kt
@@ -1,109 +1,197 @@
-package com.pemrogandroid.catatantempat.viewmodel
+package com.pemrogandroid.catatantempat.ui
 
-import android.app.Application
-import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import com.pemrogandroid.catatantempat.model.Bookmark
-import com.pemrogandroid.catatantempat.repository.BookmarkRepo
+import android.net.Uri
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.databinding.DataBindingUtil
+import com.pemrogandroid.catatantempat.R
+import com.pemrogandroid.catatantempat.databinding.ActivityBookmarkDetailsBinding
 import com.pemrogandroid.catatantempat.util.ImageUtils
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.pemrogandroid.catatantempat.viewmodel.BookmarkDetailsViewModel
+import java.io.File
+import java.net.URLEncoder
 
-class BookmarkDetailsViewModel(application: Application) : AndroidViewModel(application) {
-    private val bookmarkRepo = BookmarkRepo(getApplication())
-    private var bookmarkDetailsView: LiveData<BookmarkDetailsView>? = null
+class BookmarkDetailsActivity : AppCompatActivity(){
+    private lateinit var databinding: ActivityBookmarkDetailsBinding
+    private val bookmarkDetailsViewModel by viewModels<BookmarkDetailsViewModel>()
+    private var bookmarkDetailsView: BookmarkDetailsViewModel.BookmarkDetailsView? = null
+    private var photoFile: File? = null
 
-    fun getBookmark(bookmarkId: Long): LiveData<BookmarkDetailsView>? {
-        if (bookmarkDetailsView == null) {
-            mapBookmarkToBookmarkView(bookmarkId)
-        }
-        return bookmarkDetailsView
-    }
-
-    fun mapBookmarkToBookmarkView(bookmarkId: Long) {
-        val bookmark = bookmarkRepo.getLiveBookmark(bookmarkId)
-        bookmarkDetailsView = Transformations.map(bookmark) { repoBookmark ->
-            repoBookmark?.let { repoBookmark ->
-                bookmarkToBookmarkView(repoBookmark)
-            }
-        }
-    }
-
-    fun updateBookmark(bookmarkView: BookmarkDetailsView) {
-        GlobalScope.launch {
-            val bookmark = bookmarkViewToBookmark(bookmarkView)
-            bookmark?.let { bookmarkRepo.updateBookmark(it) }
-        }
-    }
-
-    private fun bookmarkViewToBookmark(bookmarkView: BookmarkDetailsView): Bookmark? {
-        val bookmark = bookmarkView.id?.let {
-            bookmarkRepo.getBookmark(it)
-        }
-        if (bookmark != null) {
-            bookmark.id = bookmarkView.id
-            bookmark.name = bookmarkView.name
-            bookmark.phone = bookmarkView.phone
-            bookmark.address = bookmarkView.address
-            bookmark.notes = bookmarkView.notes
-            bookmark.category = bookmarkView.category
-
-        }
-        return bookmark
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_bookmark_details)
+        setupToolbar()
+        getIntentData()
     }
 
 
-    fun deleteBookmark(bookmarkDetailsView: BookmarkDetailsView) {
-        GlobalScope.launch {
-            val bookmark = bookmarkDetailsView.id?.let {
-                bookmarkRepo.getBookmark(it)
-            }
-            bookmark?.let {
-                bookmarkRepo.deleteBookmark(it)
-            }
-        }
+    override fun onCreateOptionsMenu(menu: android.view.Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_bookmark_details, menu)
+        return true
     }
 
-    private fun bookmarkToBookmarkView(bookmark: Bookmark): BookmarkDetailsView {
-        return BookmarkDetailsView(
-            bookmark.id,
-            bookmark.name,
-            bookmark.phone,
-            bookmark.address,
-            bookmark.notes,
-            bookmark.category,
-            bookmark.longitude,
-            bookmark.latitude,
-            bookmark.placeId
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_save -> {
+            saveChanges()
+            true
+        }
+        R.id.action_delete -> {
+            deleteBookmark()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun getIntentData() {
+        val bookmarkId = intent.getLongExtra(
+            MapsActivity.EXTRA_BOOKMARK_ID, 0
         )
+        bookmarkDetailsViewModel.getBookmark(bookmarkId)?.observe(this, {
+            it?.let {
+                bookmarkDetailsView = it
+                databinding.bookmarkDetailsView = it
+                populateImageView()
+            }
+        })
     }
 
-    data class BookmarkDetailsView(
-        var id: Long? = null,
-        var name: String = "",
-        var phone: String = "",
-        var address: String = "",
-        var notes: String = "",
-        var category: String = "",
-        var longitude: Double = 0.0,
-        var latitude: Double = 0.0,
-        var placeId: String? = null
-    ) {
+    private fun setupToolbar() {
+        setSupportActionBar(databinding.toolbar)
+    }
 
-        fun getImage(context: Context) = id?.let {
-            ImageUtils.loadBitmapFromFile(context, Bookmark.generateImageFilename(it))
-        }
-
-        fun setImage(context: Context, image: Bitmap) {
-            id?.let {
-                ImageUtils.saveBitmapToFile(
-                    context, image,
-                    Bookmark.generateImageFilename(it)
-                )
+    private fun populateImageView() {
+        bookmarkDetailsView?.let { bookmarkView ->
+            val placeImage = bookmarkView.getImage(this)
+            placeImage?.let {
+                databinding.imageViewPlace.setImageBitmap(placeImage)
+            }
+            databinding.imageViewPlace.setOnClickListener {
             }
         }
+    }
+
+
+    private fun getImageWithAuthority(uri: Uri) = ImageUtils.decodeUriStreamToSize(
+        uri,
+        resources.getDimensionPixelSize(R.dimen.default_image_width),
+        resources.getDimensionPixelSize(R.dimen.default_image_height),
+        this
+    )
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == android.app.Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CAPTURE_IMAGE -> {
+                    val photoFile = photoFile ?: return
+                    val uri = FileProvider.getUriForFile(
+                        this,
+                        "com.raywenderlich.placebook.fileprovider",
+                        photoFile
+                    )
+                    revokeUriPermission(
+                        uri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                    )
+                    val image = getImageWithPath(photoFile.absolutePath)
+                    val bitmap = ImageUtils.rotateImageIfRequired(this, image, uri)
+                    updateImage(bitmap)
+                }
+
+                REQUEST_GALLERY_IMAGE -> if (data != null && data.data != null) {
+                    val imageUri = data.data as Uri
+                    val image = getImageWithAuthority(imageUri)
+                    image?.let {
+                        val bitmap = ImageUtils.rotateImageIfRequired(this, it, imageUri)
+                        updateImage(bitmap)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getImageWithPath(filePath: String) = ImageUtils.decodeFileToSize(
+        filePath,
+        resources.getDimensionPixelSize(R.dimen.default_image_width),
+        resources.getDimensionPixelSize(R.dimen.default_image_height)
+    )
+
+    private fun updateImage(image: Bitmap) {
+        bookmarkDetailsView?.let {
+            databinding.imageViewPlace.setImageBitmap(image)
+            it.setImage(this, image)
+        }
+    }
+
+
+    private fun saveChanges() {
+        val name = databinding.editTextName.text.toString()
+        if (name.isEmpty()) {
+            return
+        }
+        bookmarkDetailsView?.let { bookmarkView ->
+            bookmarkView.name = databinding.editTextName.text.toString()
+            bookmarkView.notes = databinding.editTextNotes.text.toString()
+            bookmarkView.address = databinding.editTextAddress.text.toString()
+            bookmarkView.phone = databinding.editTextPhone.text.toString()
+            bookmarkDetailsViewModel.updateBookmark(bookmarkView)
+        }
+        finish()
+    }
+
+    private fun deleteBookmark() {
+        val bookmarkView = bookmarkDetailsView ?: return
+
+        AlertDialog.Builder(this)
+            .setMessage("Delete?")
+            .setPositiveButton("Ok") { _, _ ->
+                bookmarkDetailsViewModel.deleteBookmark(bookmarkView)
+                finish()
+            }
+            .setNegativeButton("Cancel", null)
+            .create().show()
+    }
+
+    private fun sharePlace() {
+        val bookmarkView = bookmarkDetailsView ?: return
+        var mapUrl = ""
+        if (bookmarkView.placeId == null) {
+            // 3
+            val location = URLEncoder.encode(
+                "${bookmarkView.latitude},"
+                        + "${bookmarkView.longitude}", "utf-8"
+            )
+            mapUrl = "https://www.google.com/maps/dir/?api=1" +
+                    "&destination=$location"
+        } else {
+            val name = URLEncoder.encode(bookmarkView.name, "utf-8")
+            mapUrl = "https://www.google.com/maps/dir/?api=1" +
+                    "&destination=$name&destination_place_id=" +
+                    "${bookmarkView.placeId}"
+        }
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Check out ${bookmarkView.name} at:\n$mapUrl"
+        )
+        sendIntent.putExtra(
+            Intent.EXTRA_SUBJECT,
+            "Sharing ${bookmarkView.name}"
+        )
+        sendIntent.type = "text/plain"
+        startActivity(sendIntent)
+    }
+
+
+    companion object {
+        private const val REQUEST_CAPTURE_IMAGE = 1
+        private const val REQUEST_GALLERY_IMAGE = 2
     }
 }
