9e9aa4ed30ad2e76bf57e5f8ddacff6e5288fb65
Andreas Nugroho <79098402+AndreasNugoho@users.noreply.github.com>
2022-11-22 14:44:11 +0700

    pertemuan 9 dan 10 bookmark

--- catatanTempat/app/src/main/java/com/pemrogandroid/catatantempat/ui/BookmarkDetailsActivity.kt
+++ catatanTempat/app/src/main/java/com/pemrogandroid/catatantempat/ui/BookmarkDetailsActivity.kt
@@ -1,200 +1,109 @@
-package com.pemrogandroid.catatantempat.ui
+package com.pemrogandroid.catatantempat.viewmodel
 
-class BookmarkDetailsActivity : AppCompatActivity(), PhotoOptionDialogFragment.PhotoOptionDialogListener {
-    private lateinit var databinding: ActivityBookmarkDetailsBinding
-    private val bookmarkDetailsViewModel by viewModels<BookmarkDetailsViewModel>()
-    private var bookmarkDetailsView: BookmarkDetailsViewModel.BookmarkDetailsView? = null
-    private var photoFile: File? = null
+import android.app.Application
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.pemrogandroid.catatantempat.model.Bookmark
+import com.pemrogandroid.catatantempat.repository.BookmarkRepo
+import com.pemrogandroid.catatantempat.util.ImageUtils
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-    override fun onCreate(savedInstanceState: android.os.Bundle?) {
-        super.onCreate(savedInstanceState)
-        databinding = DataBindingUtil.setContentView(this, R.layout.activity_bookmark_details)
-        setupToolbar()
-        getIntentData()
-        setupFab()
-    }
-    private fun setupToolbar() {
-        setSupportActionBar(databinding.toolbar)
-    }
+class BookmarkDetailsViewModel(application: Application) : AndroidViewModel(application) {
+    private val bookmarkRepo = BookmarkRepo(getApplication())
+    private var bookmarkDetailsView: LiveData<BookmarkDetailsView>? = null
 
-    private fun populateImageView() {
-        bookmarkDetailsView?.let {
-                bookmarkView -> val placeImage = bookmarkView.getImage(this)
-            placeImage?.let {
-                databinding.imageViewPlace.setImageBitmap(placeImage)
-            }
+    fun getBookmark(bookmarkId: Long): LiveData<BookmarkDetailsView>? {
+        if (bookmarkDetailsView == null) {
+            mapBookmarkToBookmarkView(bookmarkId)
         }
-        databinding.imageViewPlace.setOnClickListener {
-            replaceImage()
-        }
+        return bookmarkDetailsView
     }
 
-    private fun getIntentData() {
-        val bookmarkId = intent.getLongExtra(MapsActivity.Companion.EXTRA_BOOKMARK_ID, 0)
-        bookmarkDetailsViewModel.getBookmark(bookmarkId)?.observe(this, {
-            it?.let {
-                bookmarkDetailsView = it
-                databinding.bookmarkDetailsView = it
-                populateImageView()
-                populateCategoryList()
-            }
-        })
-    }
-
-    override fun onCreateOptionsMenu(menu: android.view.Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_bookmark_details, menu)
-        return true
-    }
-
-    private fun saveChanges() {
-        val name = databinding.editTextName.text.toString()
-        if (name.isEmpty()) {
-            return
-        }
-        bookmarkDetailsView?.let { bookmarkView ->
-            bookmarkView.name = databinding.editTextName.text.toString()
-            bookmarkView.notes = databinding.editTextNotes.text.toString()
-            bookmarkView.address = databinding.editTextAddress.text.toString()
-            bookmarkView.phone = databinding.editTextPhone.text.toString()
-            bookmarkView.category = databinding.spinnerCategory.selectedItem as String
-            bookmarkDetailsViewModel.updateBookmark(bookmarkView)
-        }
-        finish()
-    }
-
-    override fun onCaptureClick() {
-        photoFile = null
-        try {
-            photoFile = ImageUtils.createUniqueImageFile(this)
-        } catch (ex: java.io.IOException) {
-            return
-        }
-        photoFile?.let { photoFile ->
-            val photoUri = FileProvider.getUriForFile(this, "com.raywenderlich.placebook.fileprovider", photoFile)
-            val captureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            captureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoUri)
-            val intentActivities = packageManager.queryIntentActivities(captureIntent, PackageManager.MATCH_DEFAULT_ONLY)
-            intentActivities.map { it.activityInfo.packageName}.forEach {grantUriPermission(it, photoUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)}
-            startActivityForResult(captureIntent, REQUEST_CAPTURE_IMAGE)
-        }
-    }
-
-    override fun onPickClick() {
-        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(pickIntent, REQUEST_GALLERY_IMAGE)
-    }
-
-    private fun replaceImage() {
-        val newFragment = PhotoOptionDialogFragment.newInstance(this)
-        newFragment?.show(supportFragmentManager, "photoOptionDialog")
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean =
-        when (item.itemId) {
-            R.id.action_save -> {
-                saveChanges()
-                true
-            }
-            R.id.action_delete -> {
-                deleteBookmark()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-
-    private fun updateImage(image: Bitmap) {
-        bookmarkDetailsView?.let {
-            databinding.imageViewPlace.setImageBitmap(image)
-            it.setImage(this, image)
-        }
-    }
-
-    private fun getImageWithPath(filePath:String) = ImageUtils.decodeFileToSize(filePath, resources.getDimensionPixelSize(R.dimen.default_image_width), resources.getDimensionPixelSize(R.dimen.default_image_height))
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == android.app.Activity.RESULT_OK) {
-            when (requestCode) {
-                REQUEST_CAPTURE_IMAGE -> {
-                    val photoFile = photoFile ?: return
-                    val uri = FileProvider.getUriForFile(this, "com.raywenderlich.placebook.fileprovider", photoFile)
-                    revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                    val image = getImageWithPath(photoFile.absolutePath)
-                    val bitmap = ImageUtils.rotateImageIfRequired(this, image, uri)
-                    updateImage(bitmap)
-                }
-                REQUEST_GALLERY_IMAGE -> if (data != null && data.data != null) {
-                    val imageUri = data.data as Uri
-                    val image = getImageWithAuthority(imageUri)
-                    image?.let {
-                        val bitmap = ImageUtils.rotateImageIfRequired(this, it, imageUri)
-                        updateImage(bitmap)
-                    }
-                }
+    fun mapBookmarkToBookmarkView(bookmarkId: Long) {
+        val bookmark = bookmarkRepo.getLiveBookmark(bookmarkId)
+        bookmarkDetailsView = Transformations.map(bookmark) { repoBookmark ->
+            repoBookmark?.let { repoBookmark ->
+                bookmarkToBookmarkView(repoBookmark)
             }
         }
     }
 
-    private fun populateCategoryList() {
-        val bookmarkView = bookmarkDetailsView ?: return
-        val resourceId = bookmarkDetailsViewModel.getCategoryResourceId(bookmarkView.category)
-        resourceId?.let { databinding.imageViewCategory.setImageResource(it) }
-        val categories = bookmarkDetailsViewModel.getCategories()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+    fun updateBookmark(bookmarkView: BookmarkDetailsView) {
+        GlobalScope.launch {
+            val bookmark = bookmarkViewToBookmark(bookmarkView)
+            bookmark?.let { bookmarkRepo.updateBookmark(it) }
+        }
+    }
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        databinding.spinnerCategory.adapter = adapter
-        val placeCategory = bookmarkView.category
-        databinding.spinnerCategory.setSelection(adapter.getPosition(placeCategory))
+    private fun bookmarkViewToBookmark(bookmarkView: BookmarkDetailsView): Bookmark? {
+        val bookmark = bookmarkView.id?.let {
+            bookmarkRepo.getBookmark(it)
+        }
+        if (bookmark != null) {
+            bookmark.id = bookmarkView.id
+            bookmark.name = bookmarkView.name
+            bookmark.phone = bookmarkView.phone
+            bookmark.address = bookmarkView.address
+            bookmark.notes = bookmarkView.notes
+            bookmark.category = bookmarkView.category
 
-        databinding.spinnerCategory.post {
-            databinding.spinnerCategory.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                    val category = parent.getItemAtPosition(position) as String
-                    val resourceId = bookmarkDetailsViewModel.getCategoryResourceId(category)
-                    resourceId?.let { databinding.imageViewCategory.setImageResource(it) }
-                }
-                override fun onNothingSelected(parent:AdapterView<*>) {
+        }
+        return bookmark
+    }
 
-                }
+
+    fun deleteBookmark(bookmarkDetailsView: BookmarkDetailsView) {
+        GlobalScope.launch {
+            val bookmark = bookmarkDetailsView.id?.let {
+                bookmarkRepo.getBookmark(it)
+            }
+            bookmark?.let {
+                bookmarkRepo.deleteBookmark(it)
             }
         }
     }
 
-    private fun deleteBookmark() {
-        val bookmarkView = bookmarkDetailsView ?: return
-        AlertDialog.Builder(this).setMessage("Delete?").setPositiveButton("OK") { _, _ ->
-            bookmarkDetailsViewModel.deleteBookmark(bookmarkView)
-            finish()
-        }.setNegativeButton("Cancel", null).create().show()
+    private fun bookmarkToBookmarkView(bookmark: Bookmark): BookmarkDetailsView {
+        return BookmarkDetailsView(
+            bookmark.id,
+            bookmark.name,
+            bookmark.phone,
+            bookmark.address,
+            bookmark.notes,
+            bookmark.category,
+            bookmark.longitude,
+            bookmark.latitude,
+            bookmark.placeId
+        )
     }
 
-    private fun sharePlace() {
-        val bookmarkView = bookmarkDetailsView ?: return
-        var mapUrl = ""
-        if (bookmarkView.placeId == null) {
-            val location = URLEncoder.encode("${bookmarkView.latitude}," + "${bookmarkView.longitude}", "utf-8")
-            mapUrl = "https://www.google.com/maps/dir/?api=1" + "&destination=$location"
-        } else {
-            val name = URLEncoder.encode(bookmarkView.name, "utf-8")
-            mapUrl = "https://www.google.com/maps/dir/?api=1" + "&destination=$name&destination_place_id=" + "${bookmarkView.placeId}"
+    data class BookmarkDetailsView(
+        var id: Long? = null,
+        var name: String = "",
+        var phone: String = "",
+        var address: String = "",
+        var notes: String = "",
+        var category: String = "",
+        var longitude: Double = 0.0,
+        var latitude: Double = 0.0,
+        var placeId: String? = null
+    ) {
+
+        fun getImage(context: Context) = id?.let {
+            ImageUtils.loadBitmapFromFile(context, Bookmark.generateImageFilename(it))
         }
-        val sendIntent = Intent()
-        sendIntent.action = Intent.ACTION_SEND
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "Check out ${bookmarkView.name} at:\n$mapUrl")
-        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Sharing ${bookmarkView.name}")
-        sendIntent.type = "text/plain"
-        startActivity(sendIntent)
-    }
 
-    private fun setupFab() {
-        databinding.fab.setOnClickListener { sharePlace() }
-    }
-
-    private fun getImageWithAuthority(uri: Uri) = ImageUtils.decodeUriStreamToSize(uri, resources.getDimensionPixelSize(R.dimen.default_image_width), resources.getDimensionPixelSize(R.dimen.default_image_height), this)
-
-    companion object {
-        private const val REQUEST_CAPTURE_IMAGE = 1
-        private const val REQUEST_GALLERY_IMAGE = 2
+        fun setImage(context: Context, image: Bitmap) {
+            id?.let {
+                ImageUtils.saveBitmapToFile(
+                    context, image,
+                    Bookmark.generateImageFilename(it)
+                )
+            }
+        }
     }
 }
