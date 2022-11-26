0511598ea1f3a3d13c1b3b5e354a51e3cbadfc10
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2022-11-26 21:25:24 +0700

    pertemuan 10 <viewbind room and fetch api>

---
+++ MovieKu/app/src/main/java/com/pemrogandroid/movieku/repository/RemoteDataSource.kt
@@ -0,0 +1,17 @@
+package com.pemrogandroid.movieku.repository
+
+import com.pemrogandroid.movieku.model.TmdbResponse
+import com.pemrogandroid.movieku.network.RetrofitClient
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+open class RemoteDataSource {
+
+  fun searchResultsObservable(query: String): Observable<TmdbResponse> {
+    return RetrofitClient.moviesApi
+        .searchMovie(RetrofitClient.API_KEY, query)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+  }
+}
